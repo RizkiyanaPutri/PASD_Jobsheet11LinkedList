04ac26bb85df6c0ed2457a19ef82972775126bc0
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2026-05-06 10:50:24 +0700

    praktikum 2

--- SLLMain21.java
+++ SLLMain21.java
@@ -1,28 +1,41 @@
-import java.util.Scanner;
-
 public class SLLMain21 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         SingleLinkedList21 list = new SingleLinkedList21();
+
+        Mahasiswa21 m1 = new Mahasiswa21("21212203", "Dirga", "4D", 3.6);
+        Mahasiswa21 m2 = new Mahasiswa21("23212201", "Bima", "2B", 3.8);
+        Mahasiswa21 m3 = new Mahasiswa21("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa21 m4 = new Mahasiswa21("24212200", "Alvaro", "1A", 4.0);
+
+        list.addLast(m1);
+        list.addLast(m2);
+        list.addLast(m3);
+        list.addLast(m4);
+
+        System.out.println("Isi Linked List:");
         list.print();
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int n = sc.nextInt();
-        sc.nextLine(); 
-        for (int i = 0; i < n; i++) {
-            System.out.println("\nData Mahasiswa ke-" + (i + 1));
-            System.out.print("NIM   : ");
-            String nim = sc.nextLine();
-            System.out.print("Nama  : ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas : ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK   : ");
-            double ipk = sc.nextDouble();
-            sc.nextLine(); 
-            Mahasiswa21 mhs = new Mahasiswa21(nim, nama, kelas, ipk);
-            list.addLast(mhs);
-            list.print();
-        }
-        sc.close();
+
+        System.out.println("Index data Cintia: " + list.indexOf("22212202"));
+
+        System.out.println("Data pada index ke-2:");
+        list.getData(2).tampil();
+        System.out.println();
+
+        System.out.println("Hapus data pertama:");
+        list.removeFirst();
+        list.print();
+
+        System.out.println("Hapus data terakhir:");
+        list.removeLast();
+        list.print();
+
+        System.out.println("Hapus data (Bima):");
+        list.remove("23212201");
+        list.print();
+
+        System.out.println("Hapus data index ke-0:");
+        list.removeAt(0);
+        list.print();
     }
 }
--- SingleLinkedList21.java
+++ SingleLinkedList21.java
@@ -4,31 +4,18 @@ public class SingleLinkedList21 {
     public boolean isEmpty() {
         return head == null;
     }
-
     public void print() {
-        if (isEmpty()) {
-            System.out.println("Linked List kosong\n");
-        } else {
-            System.out.println("Isi Linked List:");
-            Node21 temp = head;
-            while (temp != null) {
-                temp.data.tampil();
-                temp = temp.next;
-            }
-            System.out.println();
+    if (isEmpty()) {
+        System.out.println("Linked List kosong\n");
+    } else {
+        Node21 temp = head;
+        while (temp != null) {
+            temp.data.tampil();
+            temp = temp.next;
         }
+        System.out.println();
     }
-
-    public void addFirst(Mahasiswa21 data) {
-        Node21 newNode = new Node21(data, null);
-        if (isEmpty()) {
-            head = tail = newNode;
-        } else {
-            newNode.next = head;
-            head = newNode;
-        }
-    }
-
+}
     public void addLast(Mahasiswa21 data) {
         Node21 newNode = new Node21(data, null);
         if (isEmpty()) {
@@ -38,34 +25,83 @@ public class SingleLinkedList21 {
             tail = newNode;
         }
     }
-
-    public void insertAfter(String key, Mahasiswa21 data) {
+    public int indexOf(String nim) {
         Node21 temp = head;
+        int index = 0;
+
         while (temp != null) {
-            if (temp.data.nim.equals(key)) {
-                Node21 newNode = new Node21(data, temp.next);
-                temp.next = newNode;
-                if (temp == tail) tail = newNode;
-                break;
+            if (temp.data.nim.equals(nim)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+    public Mahasiswa21 getData(int index) {
+        Node21 temp = head;
+
+        for (int i = 0; i < index; i++) {
+            if (temp == null) return null;
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked List kosong");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked List kosong");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node21 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+    public void remove(String nim) {
+        if (isEmpty()) return;
+
+        if (head.data.nim.equals(nim)) {
+            removeFirst();
+            return;
+        }
+        Node21 temp = head;
+        while (temp.next != null) {
+            if (temp.next.data.nim.equals(nim)) {
+                temp.next = temp.next.next;
+
+                if (temp.next == null) {
+                    tail = temp;
+                }
+                break; // penting!
             }
             temp = temp.next;
         }
     }
-
-    public void insertAt(int index, Mahasiswa21 data) {
+    public void removeAt(int index) {
         if (index == 0) {
-            addFirst(data);
+            removeFirst();
             return;
         }
-
         Node21 temp = head;
         for (int i = 0; i < index - 1; i++) {
             temp = temp.next;
         }
-
-        Node21 newNode = new Node21(data, temp.next);
-        temp.next = newNode;
-
-        if (newNode.next == null) tail = newNode;
+        temp.next = temp.next.next;
+        if (temp.next == null) {
+            tail = temp;
+        }
     }
 }
